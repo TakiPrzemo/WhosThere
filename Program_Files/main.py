17e8f15d2fb04d2c9bf83893c7151f2d40434123
Przemek Kaczorowski <serdalaberda@gmail.com>
2024-05-12 19:26:07 +0200

    Update main.py - implement reading of the .yaml file and putting the data into proper known_face_names/known_face_encodings containers

--- Program_Files/main.py
+++ Program_Files/main.py
@@ -15,3 +15,29 @@ if __name__ == '__main__':
     print("Change pushed via Pycharm to git");
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+while True:
+    ret, frame = video_capture.read()
+
+    if process_this_frame:
+        small_frame = cv.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = cv.cvtColor(small_frame, cv.COLOR_BGR2RGB)
+        
+        face_loc = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_loc)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "?????"
+
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+                confidence = face_confidence(face_distances[best_match_index], 0.8)
+                face_names.append(f'{name[:-2]} {confidence}')
+            else:
+                face_names.append(name)
+
+    process_this_frame = not process_this_frame
